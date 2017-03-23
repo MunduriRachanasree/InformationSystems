3fa43ff5106899dc4a5c5e604e2dade9adf5cb85
YagnaBhargavi <yagnabhargavi@gmail.com>
2017-03-23 23:52:24 +0530

    studentId field is hidden

--- Apps/StudentInformationSystem/forms.py
+++ Apps/StudentInformationSystem/forms.py
@@ -1,4 +1,5 @@
 from django.forms import ModelForm
+from django import forms
 from StudentInformationSystem.models import *
 
 class PersonalInfoForm(ModelForm):
@@ -7,13 +8,26 @@ class PersonalInfoForm(ModelForm):
 		fields = '__all__'			
 
 
-class AcedamicInfoForm(ModelForm):
+class AcedamicInfoForm(forms.ModelForm):
 	class Meta:
 		model = AcedamicInfo
-		exclude = ('studentId',)
+		fields = '__all__'
+		widgets = {'studentId': forms.HiddenInput()}
+		
+	def __init__(self, *args, **kwargs):
+		# first call parent's constructor
+		super(AcedamicInfoForm, self).__init__(*args, **kwargs)
+		# there's a `fields` property now
+		self.fields['studentId'].required = False
 		
 class AdditionalInfoForm(ModelForm):
 	class Meta:
 		model = AdditionalInfo
-		exclude = ('studentId',)
+		fields = '__all__'
+		widgets = {'studentId': forms.HiddenInput()}
+	def __init__(self, *args, **kwargs):
+		# first call parent's constructor
+		super(AdditionalInfoForm, self).__init__(*args, **kwargs)
+		# there's a `fields` property now
+		self.fields['studentId'].required = False
 					
