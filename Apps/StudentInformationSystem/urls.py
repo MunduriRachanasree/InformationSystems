43c053263268700ec64cf5b5ee0028aa8e26cbcd
Krishna-Priya <14wh1a0458@bvrithyderabad.edu.in>
2017-03-25 01:19:56 +0530

    Update urls.py

--- Apps/StudentInformationSystem/urls.py
+++ Apps/StudentInformationSystem/urls.py
@@ -2,9 +2,14 @@ from django.conf.urls import url
 from StudentInformationSystem import views
 
 urlpatterns = [ 
+	url(r'^EditProfile$',views.editStudent,name = 'EditProfile'),
 	url(r'^$',views.index,name = 'index'),
 	url(r'^base$',views.base,name = 'base'),
 	url(r'^acedamicInfo$',views.acedamicInfo,name = 'acedamicInfo'),
 	url(r'^additionalInfo$',views.additionalInfo,name = 'additionalInfo'),
-	url(r'^display$',views.display,name = 'display')
+	url(r'^success$',views.success,name = 'success'),
+	url(r'^StudentView$',views.viewStudent,name = 'StudentView'),
+	url(r'^home$',views.notifications,name = 'notifications'),
+	url(r'^resume$',views.resume,name = 'resume'),
+	url(r'^edit$',views.edit,name = ''),
 ]
