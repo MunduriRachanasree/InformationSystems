3f47eb3bdef6e7a22e74e3d291b1b7ba3a9b773d
YagnaBhargavi <yagnabhargavi@gmail.com>
2017-03-25 01:12:20 +0530

    Update views.py
    student module is updated

--- Apps/StudentInformationSystem/views.py
+++ Apps/StudentInformationSystem/views.py
@@ -25,28 +25,82 @@ def acedamicInfo(request):
 	if request.method == "POST":
 		form = AcedamicInfoForm(request.POST)
 		if form.is_valid():
-			AcedamicInfo = form.save(commit=False)
-			AcedamicInfo.save()
+			AIF = form.save(commit=False)
+			try:
+				Student = PersonalInfo.objects.get(pk = 32)
+			except PersonalInfo.DoesNotExist:
+				raise Http404("Comment does not exist")
+			AIF.studentId = Student
+			AIF.save()
 			return HttpResponseRedirect('/StudentInformationSystem/additionalInfo')
 	else:
 		form = AcedamicInfoForm()
 	return render(request, 'StudentInformationSystem/success.html', {'form': form})
 
- def additionalInfo(request):
+def additionalInfo(request):
 	if request.method == "POST":
 		form = AdditionalInfoForm(request.POST)
 		if form.is_valid():
-			AdditionalInfo = form.save(commit=False)
-			AdditionalInfo.save()
-			return redirect('success')
+			ADIF = form.save(commit=False)
+			try:
+				Student = PersonalInfo.objects.get(pk = 32)
+			except PersonalInfo.DoesNotExist:
+				raise Http404("Comment does not exist")
+			ADIF.studentId = Student
+			ADIF.save()
+			return redirect('/StudentInformationSystem/home')
 	else:
 		form = AdditionalInfoForm()
 	return render(request, 'StudentInformationSystem/additionalInfo.html', {'form': form})
 
-def display(request):
-         try:
-             Student = PersonalInfo.objects.all()
-         except PersonalInfo.DoesNotExist:
-             raise Http404("Comment does not exist")
-            
-         return render(request, "informationystem/display.html",{'Student': Student})
+def notifications(request):
+	Student = PersonalInfo.objects.get(pk = 32)
+	sug = Suggestion.objects.filter(studentId = Student)
+	notice = Notifications.objects.all()
+	return render(request, 'StudentInformationSystem/notifications.html', {'sugs': sug,'notices':notice})
+	
+def resume(request):
+	Student = PersonalInfo.objects.get(pk = 32)
+	Acedamic = AcedamicInfo.objects.get(studentId = Student)
+	Additional = AdditionalInfo.objects.get(studentId = Student)
+	return render(request, 'StudentInformationSystem/resume.html', {'p': Student,'ac':Acedamic,'ad':Additional})
+		
+def viewStudent(request):
+	Student = PersonalInfo.objects.get(pk = 32)
+	Acedamic = AcedamicInfo.objects.get(studentId = Student)
+	Additional = AdditionalInfo.objects.get(studentId = Student)
+	return render(request, 'StudentInformationSystem/StudentView.html', {'person': Student,'acedamic':Acedamic,'additional':Additional})
+	
+def editStudent(request):
+	Student = PersonalInfo.objects.get(pk = 32)
+	Acedamic = AcedamicInfo.objects.get(studentId = Student)
+	Additional = AdditionalInfo.objects.get(studentId = Student)
+	return render(request, 'StudentInformationSystem/EditProfile.html', {'person': Student,'acedamic':Acedamic,'additional':Additional})
+	
+def edit(request):
+	if request.method == "POST":
+		p = PersonalInfo.objects.get(id = 32);
+		p.first_name = request.POST['fname']
+		p.last_name = request.POST['lname']
+		p.address = request.POST['address']
+		p.email = request.POST['email']
+		p.phoneNo = request.POST['phoneno']
+		p.save()
+		q = AcedamicInfo.objects.get(studentId = p)
+		q.yearOfJoining = request.POST['yoj']
+		q.aggregate = request.POST['aggregate']
+		q.upperSecondaryInstitution = request.POST['ui']
+		q.upperSecondaryBoard = request.POST['ub']
+		q.upperSecondaryPercentage = request.POST['up']
+		q.SecondaryInstitution = request.POST['si']
+		q.SecondaryPercentage = request.POST['sp']
+		q.SecondaryBoard = request.POST['sb']
+		q.save()
+		r = AdditionalInfo.objects.get(studentId = p)
+		r.coAcademicActivities = request.POST['caa']
+		r.details = request.POST['details']
+		r.coCurriculars = request.POST['cca']
+		r.hobbies = request.POST['hobbies']
+		r.save()
+		
+		return render(request, 'StudentInformationSystem/index.html')
