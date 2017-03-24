c11ab91c4cc3faf5011a13806947b8be22756e9c
NookalaBhargavi <bhargavinookala@gmail.com>
2017-03-24 21:13:50 +0530

    Delete urls.py

--- urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from StudentInformationSystem import views
-
-urlpatterns = [ 
-	url(r'^$',views.index,name = 'index'),
-	url(r'^base$',views.base,name = 'base'),
-	url(r'^display$',views.display,name = 'display')
-]
