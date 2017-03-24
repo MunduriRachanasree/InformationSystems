36da91fd72d0030499527281b5ebf11f08575bd3
NookalaBhargavi <bhargavinookala@gmail.com>
2017-03-24 21:11:29 +0530

    Delete forms.py

--- forms.py
+++
@@ -1,7 +0,0 @@
-from django.forms import ModelForm
-from StudentInformationSystem.models import PersonalInfo
-
-class PersonInfoForm(ModelForm):
-	class Meta:
-		model = PersonalInfo
-		fields = '__all__'
